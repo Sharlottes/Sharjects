ea110f5521b0456ac5a901cf8700a7a55f77f2bc
sharlottes <seeseafood@naver.com>
2022-11-15 15:39:59 +0900

    test2

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,10 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/projects', request.url.toLowerCase()))
+  console.log(request.url);
+  return NextResponse.redirect(new URL(request.url.toLowerCase(), '/projects'))
 }
 
 export const config = {
-  matcher: '/projects/:path*([A-Z])'
+  matcher: '/projects/:path[A-Z]'
 }
