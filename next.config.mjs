dffa4c7a5b486e089ad4291c013d4b72d4803d5f
sharlottes <seeseafood@naver.com>
2022-11-20 10:27:56 +0900

    제발되라제발되라제발되라제발되라제발되라제발되라제발되라제발되라

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,16 @@ const nextConfig = {
     });
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ],
+      },
+    ]
+  },
   compiler: {
     styledComponents: true,
     emotion: true
