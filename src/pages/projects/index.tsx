724cf9af3693afb92cfdeaae8d246d734ec59a4e
sharlottes <seeseafood@naver.com>
2022-12-10 20:58:51 +0900

    더 반응적인 project 페이지

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -16,6 +16,83 @@ import { projectDataType } from 'src/@type'
 import GithubIcon from 'src/assets/icons/GithubIcon';
 import GithubRepoCardFetcher from 'src/components/GithubRepoCard';
 
+import { motion } from 'framer-motion'
+import { styled } from '@mui/system';
+
+const ProjectsContainer = styled(motion.div)(({ theme }) => ({
+  display: 'grid',
+  [theme.breakpoints.between('xs', 'sm')]: {
+    gridTemplateColumns: 'repeat(1, 1fr)'
+  },
+  [theme.breakpoints.between('sm', 'md')]: {
+    gridTemplateColumns: 'repeat(2, 1fr)'
+  },
+  [theme.breakpoints.between('md', 'lg')]: {
+    gridTemplateColumns: 'repeat(3, 1fr)'
+  },
+  [theme.breakpoints.between('lg', 1400)]: {
+    gridTemplateColumns: 'repeat(4, 1fr)'
+  },
+  [theme.breakpoints.between(1400, 'xl')]: {
+    gridTemplateColumns: 'repeat(5, 1fr)'
+  },
+  [theme.breakpoints.up('xl')]: {
+    gridTemplateColumns: 'repeat(6, 1fr)'
+  },
+}))
+
+const ProjectCard = styled(Card)(({ theme }) => ({
+  borderRadius: '20px',
+  border: '1px solid #dcdcdc',
+  minWidth: '250px',
+  margin: '10px',
+  transition: 'all 250ms ease-in',
+  display: 'grid', flexDirection: 'column', justifyContent: 'space-between', gridTemplateColumns: '1fr',
+  "& .MuiDivider-root": {
+    transition: 'width,background-color',
+    transitionDuration: '250ms',
+    transitionDelay: '0ms,250ms',
+    width: 0,
+    backgroundColor: 'gray'
+  },
+  "& .link-btn": {
+    opacity: 0,
+    transition: 'opacity,color',
+    transitionDuration: '500ms',
+    "&:hover": {
+      color: 'white',
+    },
+    [theme.breakpoints.down('md')]: {
+      color: 'white',
+      "&::before": {
+        opacity: 1
+      }
+    },
+  },
+  "& .collapse-bar": {
+    opacity: 0,
+    transform: 'translateY(100%)',
+    transition: 'opacity,transform',
+    transitionDuration: '100ms,500ms',
+    transitionDelay: '125ms',
+    transitionTimingFunction: 'ease,cubic-bezier(.01,1.76,.67,.79)',
+  },
+  "&:hover": {
+    boxShadow: '0 0 10px black',
+    "& .MuiDivider-root": {
+      width: '100%',
+      backgroundColor: '#a9d8ff'
+    },
+    "& .link-btn": {
+      opacity: 1,
+    },
+    "& .collapse-bar": {
+      opacity: 1,
+      transform: 'translateY(0%)'
+    }
+  }
+}))
+
 const projectData: projectDataType['projects'] = (require('components/pages/projectData.json') as projectDataType[]).map(data => data.projects).flat();
 
 const Projects: React.FC = () => (
@@ -24,60 +101,14 @@ const Projects: React.FC = () => (
       <div style={{ display: 'flex', justifyContent: 'center', width: '100%', fontSize: 50, fontWeight: 'bold' }}>
         Projects
       </div>
-      <div style={{ width: '100%', textAlign: 'center', padding: '020px' }}>
+      <div style={{ width: '100%', textAlign: 'center', padding: '20px' }}>
         여태껏 개발해온 개인 또는 팀 주관의 다양한 언어와 라이브러리, 프레임워크를 겸비한 토이, 사이드, 메인 프로젝트들의 목록이에요!
       </div>
     </div>
     <div style={{ margin: '50px 0' }}>
-      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
+      <ProjectsContainer layout>
         {projectData.map(data => (
-          <Card sx={{
-            width: 'min(400px, calc(100% / 2 - 20px))',
-            borderRadius: '20px',
-            border: '1px solid #dcdcdc',
-            margin: '10px',
-            transition: 'all 250ms ease-in',
-            display: 'grid',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            "& .MuiDivider-root": {
-              transition: 'width,background-color',
-              transitionDuration: '250ms',
-              transitionDelay: '0ms,250ms',
-              width: 0,
-              backgroundColor: 'gray'
-            },
-            "& .link-btn": {
-              opacity: 0,
-              transition: 'opacity,color',
-              transitionDuration: '500ms',
-              "&:hover": {
-                color: 'white',
-              }
-            },
-            "& .collapse-bar": {
-              opacity: 0,
-              transform: 'translateY(100%)',
-              transition: 'opacity,transform',
-              transitionDuration: '100ms,500ms',
-              transitionDelay: '125ms',
-              transitionTimingFunction: 'ease,cubic-bezier(.01,1.76,.67,.79)',
-            },
-            "&:hover": {
-              boxShadow: '0 0 10px black',
-              "& .MuiDivider-root": {
-                width: '100%',
-                backgroundColor: '#a9d8ff'
-              },
-              "& .link-btn": {
-                opacity: 1,
-              },
-              "& .collapse-bar": {
-                opacity: 1,
-                transform: 'translateY(0%)'
-              }
-            }
-          }}>
+          <ProjectCard key={data.name}>
             <div style={{
               padding: '10px',
               gridColumnStart: 1, gridRowStart: 1
@@ -135,15 +166,19 @@ const Projects: React.FC = () => (
               </div>
             </div>
             {!data.noGithub && <CollapseBar projectName={data.name} />}
-          </Card>
+          </ProjectCard>
         ))}
-      </div>
+      </ProjectsContainer>
     </div>
   </Layout >
 )
 
 const CollapseBar: React.FC<{ projectName: string }> = ({ projectName }) => {
   const [opened, setOpened] = React.useState(false);
+
+  const handleClick = () => {
+    setOpened(prev => !prev);
+  }
 
   return (
     <div className="collapse-bar" style={{
@@ -153,14 +188,14 @@ const CollapseBar: React.FC<{ projectName: string }> = ({ projectName }) => {
       gridColumnStart: 1, gridRowStart: 1, alignSelf: 'flex-end',
       pointerEvents: 'none'
     }}>
-      <IconButton onClick={() => setOpened(prev => !prev)} sx={{ pointerEvents: 'fill', transition: 'all 250ms ease', transform: `translateY(${opened ? '-15%' : 'auto'})` }} >
+      <IconButton onClick={handleClick} sx={{ pointerEvents: 'fill', transition: 'all 250ms ease', transform: `translateY(${opened ? '-15%' : 'auto'})` }} >
         <KeyboardDoubleArrowUpIcon sx={{ transition: 'all 250ms ease 100ms', transform: `rotate(${opened ? '180deg' : 0})` }} />
       </IconButton>
       <Slide direction="up" in={opened} unmountOnExit mountOnEnter>
         <div className='collapse-body' style={{ backgroundColor: 'black', padding: '15px 10px 10px 10px', boxShadow: 'inset 0 7px 7px #777777' }}>
           <div style={{ display: 'flex' }}>
             <GithubIcon sx={{ margin: '10px' }} />
-            <span>
+            <span style={{ textAlign: 'left' }}>
               이 프로젝트는 깃허브 레포지토리가 있습니다!<br />
               <strong>한번 둘러보시고 좋으면 Star를 눌러주세요!</strong>
             </span>
