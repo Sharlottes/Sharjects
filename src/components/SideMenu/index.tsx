949bff970f0395494a9b7dae1829dd8a52a6f75e
sharlottes <seeseafood@naver.com>
2022-12-11 13:59:24 +0900

    style cleanup

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
-import Divider from '@mui/material/Divider'
 import Drawer, { type DrawerProps } from '@mui/material/Drawer'
-import Dialog from '@mui/material/Dialog'
+import type { SvgIconProps } from '@mui/material/SvgIcon'
+import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 
@@ -17,213 +16,113 @@ import KakaoTalkIcon from 'src/assets/icons/KakaoTalkIcon'
 import DiscordIcon from 'src/assets/icons/DiscordIcon'
 import GithubIcon from 'src/assets/icons/GithubIcon'
 import VelogIcon from 'src/assets/icons/VelogIcon'
+
 import type { projectDataType } from 'src/@type'
+import { OwnerRow, ProjectRow, LinksContainer } from './styled'
 import { useThemeController } from '../MainThemeProvider'
-
-import VisitorGraphDialog from './VisitorGraphDialog'
-
-const date = new Date();
-const dateCode = `${date.getFullYear()}${(date.getMonth() + 1).toString().padStart(2, "0")}${date.getDate().toString().padStart(2, "0")}`;
+import Status from './Status'
 
 const projectData: Array<projectDataType> = require('public/data/projectData.json');
+const links: Array<[string, React.FC<SvgIconProps>] | [string, React.FC<SvgIconProps>, string]> = [
+    ['https://github.com/sharlottes', GithubIcon, 'themedBlack'],
+    ['https://discordapp.com/users/473072758629203980', DiscordIcon],
+    ['mailto:aaa9810321@gmail.com', EmailIcon, '#c5221f'],
+    ['https://velog.io/@sharlotte_04', VelogIcon],
+    ['https://open.kakao.com/o/sJxW8TUb', KakaoTalkIcon, '#black'],
+]
 
-const Status: React.FC = () => {
-    const [visitors, setVisitors] = React.useState<Record<string, number>>();
-    const [dialogOpen, setDialogOpen] = React.useState(false);
-
-    React.useEffect(() => {
-        fetch('/api/visit')
-            .then(res => res.json())
-            .then(data => setVisitors(data));
-    }, []);
-
-    return (
-        <div style={{ marginBottom: '10px' }}>
-            <Divider textAlign='left'><Typography fontWeight={500} fontSize={12}>Visitors</Typography></Divider>
-            <div style={{ marginLeft: '10px', fontSize: 12, fontWeight: 500 }}>
-                {visitors
-                    ? <>
-                        이 사이트는 오늘 {visitors[dateCode] || 0}번 조회되었고,<br />
-                        총 {Object.values(visitors).reduce((a, e) => a + e, 0)}번 조회되었어요.
-                    </>
-                    : <>방문자 불러오는 중...</>
-                }
-                <span style={{ display: 'inline', color: 'blue', cursor: 'pointer' }} onClick={() => setDialogOpen(true)}>
-                    그래프 보기
-                </span>
-            </div>
-            {visitors
-                ?
-                <VisitorGraphDialog
-                    data={visitors}
-                    maxWidth='xs' fullWidth
-                    onClose={() => setDialogOpen(false)}
-                    open={dialogOpen}
-                />
-                :
-                <Dialog
-                    onClose={() => setDialogOpen(false)}
-                    open={dialogOpen}
-                >
-                    <Box sx={{ padding: '15px' }}>
-                        <Typography variant='h1' noWrap fontWeight='bold' fontSize={25} textAlign='center' sx={{ margin: '5px' }}>알림!</Typography>
-                        <Divider />
-                        <Box sx={{ margin: '10px' }}>
-                            <Typography variant='h3' noWrap fontWeight={700} fontSize={15} textAlign='center'>방문자 데이터를 불러오는 중이에요!</Typography>
-                            <Typography variant='body1' noWrap fontWeight={400} textAlign='center' sx={{ marginTop: '5px' }}>잠시만 기다려주세요...</Typography>
-                        </Box>
-                    </Box>
-                </Dialog>
-            }
-        </div>
-    )
-}
+const DivTypography: React.FC<{ title?: string }> = ({ title }) => (
+    <Divider textAlign='left' sx={{ margin: '10px 0' }}>
+        {title && <Typography fontWeight={500} fontSize={12}>{title}</Typography>}
+    </Divider>
+)
 
 const SideMenuDrawer: React.FC<DrawerProps> = (props) => {
-    let { currentColors } = useThemeController();
+    const { currentColors } = useThemeController();
 
     return (
         <Drawer {...props} PaperProps={{
             sx: {
-                display: 'flex', flexDirection: 'column', justifyContent: "space-between", overflowX: 'hidden'
+                display: 'flex', flexDirection: 'column', overflowX: 'hidden',
+                padding: '0 24px',
+                "& >div": {
+                    width: '100%'
+                }
             }
         }}>
-            <div style={{ padding: '0 24px', marginTop: '20px' }}>
-                <div style={{ marginBottom: '10px', width: '100' }}>
-                    <a href='/'>
-                        <Typography sx={{ fontWeight: 800, fontSize: 20, textAlign: 'left', transition: 'color 150ms ease-in', "&:hover": { color: currentColors[600] } }}>
-                            Sharlotte's Portfolio
-                        </Typography>
-                    </a>
-                    <Typography variant='body2' sx={{ marginLeft: '5px', position: 'relative', left: '100px' }}>the first portfolio</Typography>
-                </div>
-
-                <Divider />
-
-                <div>
-                    <a href='/timeline'>
-                        <Button startIcon={<HistoryIcon />} variant='contained' size='small' sx={{ borderRadius: '20px', margin: '10px' }}>
-                            Timeline
-                        </Button>
-                    </a>
-                    <a href='/projects'>
-                        <Button startIcon={<SourceIcon />} variant='contained' size='small' sx={{ borderRadius: '20px', margin: '10px' }}>
-                            Projects
-                        </Button>
-                    </a>
-                </div>
-
-                <Divider textAlign='left'><Typography fontWeight={500} fontSize={12}>Projects</Typography></Divider>
-
-                <div style={{ marginLeft: '10px', marginTop: '15px', width: '100%' }}>
-                    {projectData.map(({ owner, projects }) =>
-                        <div key={owner} style={{ marginTop: '10px' }}>
-                            <Box className='highlight' sx={{
-                                display: 'flex', justifyItems: 'start',
-                                "&:hover": {
-                                    "& p": {
-                                        color: currentColors[600]
-                                    }
-                                }
-                            }}>
-                                <img src={`/images/profile/${owner}.png`} width='20px' height='20px' alt='' style={{ borderRadius: '20px', marginRight: '5px' }} />
-                                <a href={`https://github.com/${owner}`}>
-                                    <Typography sx={{
-                                        fontWeight: 500, fontsize: 16, textAlign: 'left',
-                                        transition: 'color 150ms ease-in'
-                                    }}>{owner}</Typography>
-                                </a>
-                            </Box>
-                            {projects.map((project, i) => (
-                                <div key={i} style={{ display: 'flex', justifyContent: 'space-between' }}>
-                                    <Box className='highlight' sx={{
-                                        "&:hover": {
-                                            "& p": {
-                                                color: currentColors[300],
-                                            },
-                                            marginLeft: '50px'
-                                        },
-                                        transition: 'all 100ms ease',
-                                        marginLeft: '35px',
-                                        marginTop: '3px', marginBottom: '3px',
-                                        display: 'flex', alignItems: 'center'
-                                    }}>
-                                        <img src={`/images/icon/${project.icon}.png`} alt="" style={{ height: '1rem', width: '1rem', marginRight: '5px' }} />
-                                        <a href={`/projects/${project.name}`}>
-                                            <Typography sx={{
-                                                fontWeight: 600,
-                                                transition: 'all 100ms ease'
-                                            }}>
-                                                {project.name}
-                                            </Typography>
-                                        </a>
-                                    </Box>
-                                    <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
-                                        {!project.noGithub &&
-                                            <a href={`https://github.com/${owner}/${project.name}`}>
-                                                <GithubIcon sx={{ transform: 'scale(0.8)', color: 'text.secondary', transition: 'color 300ms ease-out', "&:hover": { color: 'text.primary' } }} />
-                                            </a>
-                                        }
-                                        {project.link &&
-                                            <a href={project.link}>
-                                                <OpenInNewIcon sx={{ transform: 'scale(0.8)', color: 'text.secondary', transition: 'color 300ms ease-out', "&:hover": { color: 'text.primary' } }} />
-                                            </a>
-                                        }
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    )}
-                </div>
+            <div>
+                <a href='/'>
+                    <Typography sx={{ fontWeight: 800, fontSize: 20, transition: 'color 150ms ease-in', "&:hover": { color: currentColors[600] } }}>
+                        Sharlotte's Portfolio
+                    </Typography>
+                </a>
+                <Typography variant='body2' sx={{ marginLeft: '5px', position: 'relative', left: '100px' }}>the first portfolio</Typography>
             </div>
-            <div style={{ width: '100%' }}>
-                <Divider textAlign='left'>
-                    <Typography fontWeight={500} fontSize={12}>About</Typography>
-                </Divider>
 
-                <div style={{ margin: '20px' }}>
-                    <Status />
-                    <div>
-                        <Divider textAlign='left'><Typography fontWeight={500} fontSize={12}>Links</Typography></Divider>
+            <DivTypography />
 
-                        <Box sx={{
-                            display: 'flex',
-                            alignItems: 'end',
-                            "& a": {
-                                transition: 'margin-bottom 200ms',
-                                marginBottom: '0px',
-                                "&:hover": { marginBottom: '5px' },
-                                "& button": { color: 'inherit' }
-                            }
-                        }}>
-                            <a href='https://github.com/sharlottes'>
-                                <IconButton>
-                                    <GithubIcon sx={{ color: 'themedBlack' }} />
-                                </IconButton>
+            <div>
+                <a href='/timeline'>
+                    <Button startIcon={<HistoryIcon />} variant='contained' size='small' sx={{ borderRadius: '20px', margin: '10px' }}>
+                        Timeline
+                    </Button>
+                </a>
+                <a href='/projects'>
+                    <Button startIcon={<SourceIcon />} variant='contained' size='small' sx={{ borderRadius: '20px', margin: '10px' }}>
+                        Projects
+                    </Button>
+                </a>
+            </div>
+
+            <DivTypography title='Projects' />
+            <div style={{ margin: '15px 0 0 10px', gap: '10px' }}>
+                {projectData.map(({ owner, projects }) =>
+                    <div key={owner}>
+                        <OwnerRow color={currentColors[600]}>
+                            <img className='profile-image' src={`/images/profile/${owner}.png`} alt='' />
+                            <a href={`https://github.com/${owner}`}>
+                                <Typography className='highlight' fontWeight={500}>{owner}</Typography>
                             </a>
-                            <a href='https://discordapp.com/users/473072758629203980'>
-                                <IconButton>
-                                    <DiscordIcon />
-                                </IconButton>
-                            </a>
-                            <a href='mailto:aaa9810321@gmail.com'>
-                                <IconButton>
-                                    <EmailIcon sx={{ color: '#c5221f' }} />
-                                </IconButton>
-                            </a>
-                            <a href='https://velog.io/@sharlotte_04'>
-                                <IconButton>
-                                    <VelogIcon />
-                                </IconButton>
-                            </a>
-                            <a href='https://open.kakao.com/o/sJxW8TUb'>
-                                <IconButton>
-                                    <KakaoTalkIcon sx={{ color: 'black' }} />
-                                </IconButton>
-                            </a>
-                        </Box>
+                        </OwnerRow>
+                        {projects.map(project => (
+                            <ProjectRow key={project.name} color={currentColors[300]}>
+                                <div className='highlight'>
+                                    {project.icon
+                                        ? <img className='project-icon' src={`/images/icon/${project.icon}.png`} alt="" />
+                                        : <div className='project-icon' />
+                                    }
+                                    <a href={`/projects/${project.name.toLowerCase()}`}>
+                                        <Typography fontWeight={600}>{project.name}</Typography>
+                                    </a>
+                                </div>
+                                <div className='links'>
+                                    {!project.noGithub &&
+                                        <a href={`https://github.com/${owner}/${project.name}`}>
+                                            <GithubIcon />
+                                        </a>
+                                    }
+                                    {project.link &&
+                                        <a href={project.link}>
+                                            <OpenInNewIcon />
+                                        </a>
+                                    }
+                                </div>
+                            </ProjectRow>
+                        ))}
                     </div>
-                </div>
+                )}
+            </div>
+            <div style={{ marginTop: 'auto' }}>
+                <DivTypography title='Visitors' />
+                <Status />
+                <DivTypography title='Links' />
+                <LinksContainer>
+                    {links.map(([link, Component, color]) => (
+                        <a href={link}>
+                            <Component sx={{ color }} />
+                        </a>
+                    ))}
+                </LinksContainer>
             </div>
         </Drawer>
     )
---
+++ src/components/SideMenu/styled.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
+
+import { experimental_sx, styled } from '@mui/system'
+
+export const OwnerRow = styled('div')<{ color: string }>(({ color }) => (
+  experimental_sx({
+    display: 'flex',
+    "& .highlight": {
+      fontSize: 16,
+      transition: 'color 150ms ease-in'
+    },
+    "&:hover": {
+      "& .highlight": {
+        color
+      }
+    },
+    "& .profile-image": {
+      borderRadius: '20px', marginRight: '5px',
+      width: '20px', height: '20px'
+    },
+  })
+))
+
+export const ProjectRow = styled('div')<{ color: string }>(({ color }) => (
+  experimental_sx({
+    margin: '3px 0',
+    display: 'flex', justifyContent: 'space-between',
+    "& .highlight": {
+      transition: 'transform 100ms ease',
+      transform: 'translateX(35px)',
+      display: 'flex', alignItems: 'center',
+      "& p": {
+        transition: 'color 100ms ease',
+      },
+      "& .project-icon": {
+        width: '1rem', height: '1rem',
+        marginRight: '5px'
+      },
+    },
+    "& .links": {
+      display: 'flex', flexDirection: 'row-reverse',
+      "& .MuiSvgIcon-root": {
+        transform: 'scale(0.8)',
+        color: 'text.secondary',
+        transition: 'color 300ms ease-out',
+        "&:hover": {
+          color: 'text.primary'
+        }
+      },
+    },
+    "&:hover": {
+      "& .highlight": {
+        transform: 'translateX(50px)',
+        "& p": { color },
+      },
+    },
+  })
+))
+
+export const LinksContainer = styled('div')(
+  experimental_sx({
+    display: 'flex', justifyContent: 'space-evenly',
+    "& a": {
+      transition: 'transform 200ms',
+      transform: 'translateY(0)',
+      color: 'inherit',
+      "&:hover": { transform: 'translateY(-5px)' }
+    }
+  })
+)
