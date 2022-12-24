129affd52d1d6aaaca0e98a54598e636f01c7166
sharlottes <seeseafood@naver.com>
2022-12-25 04:11:03 +0900

    footer cleanup

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,40 +1,29 @@
-import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
 
 const Footer: React.FC<{ additional?: React.ReactNode | undefined }> = ({
   additional,
 }) => (
   <footer>
-    <Box
+    <Typography
+      variant="body2"
       sx={{
-        width: "100%",
-        minHeight: "100px",
-        backgroundColor: "#e6e6e6",
+        margin: "0 auto",
+        p: "20px 0",
+        width: "fit-content",
       }}
     >
-      <Divider />
-      <div
-        style={{
-          padding: "20px 10% 20px 10%",
-          display: "flex",
-          justifyContent: "center",
-          color: "black",
-        }}
-      >
-        <Typography variant="body2">
-          이 포트폴리오는 2년간 제작된 프로젝트들을 소개하기 위해 웹개발 공부
-          목적으로 시작된 프로젝트입니다.
-          <br />
-          로그인을 통한 모든 개인정보는 데이터베이스에 저장되며 개발 테스트를
-          위한 데이터로 이용됩니다. 사용에 주의해주세요.
-          <br />
-          <br />
-          Copyright 2022.Sharlotte. All rights reserved.
-        </Typography>
-        {additional}
-      </div>
-    </Box>
+      이 포트폴리오는 2년간 제작된 프로젝트들을 소개하기 위해 웹개발 공부
+      목적으로 시작된 프로젝트입니다.
+      <br />
+      로그인을 통한 모든 개인정보는 데이터베이스에 저장되며 개발 테스트를 위한
+      데이터로 이용됩니다. 사용에 주의해주세요.
+      <br />
+      <br />
+      <span style={{ textAlign: "center" }}>
+        Copyright 2022.Sharlotte. All rights reserved.
+      </span>
+    </Typography>
+    {additional}
   </footer>
 );
 
