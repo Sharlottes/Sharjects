6a1145bd75c7d710498e22ac5b3f78eca776e418
sharlottes <seeseafood@naver.com>
2023-01-01 05:52:13 +0900

    feat: single debounce not requires key

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,16 +1,33 @@
-type debounceType = <PT extends Array<any>, RT = void>(
+export function debounce<PT extends Array<any>, RT = void>(
   callback: (...args: PT) => RT,
   duration: number
-) => (
+): (
   key: string,
   ...params: Parameters<typeof callback>
 ) => ReturnType<typeof callback> | undefined;
-export const debounce: debounceType = (callback, duration) => {
+
+export function debounce<PT extends Array<any>, RT = void>(
+  callback: (...args: PT) => RT,
+  duration: number,
+  singleDebounce: true
+): (
+  ...params: Parameters<typeof callback>
+) => ReturnType<typeof callback> | undefined;
+
+export function debounce<PT extends Array<any>, RT = void>(
+  callback: (...args: PT) => RT,
+  duration: number,
+  singleDebounce?: true
+): (
+  key?: string,
+  ...params: Parameters<typeof callback>
+) => ReturnType<typeof callback> | undefined {
   let id: Record<string, NodeJS.Timeout | undefined> = {};
 
-  return (key, ...params) => {
+  return (key = singleDebounce ? "single" : undefined, ...params) => {
+    if (!key) throw new Error("multi debounce key should be given!");
     if (id[key]) return;
     id[key] = setTimeout(() => (id[key] = undefined), duration);
     return callback(...params);
   };
-};
+}
