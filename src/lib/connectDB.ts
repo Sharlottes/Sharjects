6d38bfa8a015277056358cb09e3baa52e1060c47
Sharlotte <seeseafood@naver.com>
2023-01-24 00:10:57 +0900

    refactor: remove logging at mongo db connecting

--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -1,19 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB = () => {
-  console.log("connecting to mongodb");
-  if (!mongoose.connections) {
-    console.log("no connections.");
-    return;
-  }
-  if (mongoose.connections[0].readyState) {
-    console.log("Already connected.");
-    return;
-  }
-  mongoose.connect(process.env.MONGODB_URI).then(
-    (res) => console.log("Successed to connect mongodb"),
-    (err) => console.log("Failed to connect mongodb.\n", err)
-  );
+  if (!mongoose.connections || mongoose.connections[0].readyState) return;
+  mongoose.connect(process.env.MONGODB_URI);
 };
 
 export default connectDB;
