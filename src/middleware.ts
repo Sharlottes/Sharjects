5e3802f43e384b38e4a4efd48aec890065a5c2f2
Sharlotte <aaa9810321@gmail.com>
2023-03-08 16:02:55 +0900

    feat: #10 middleware로 소문자 라우팅

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+const Middleware = (req: NextRequest) => {
+  if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase())
+    return NextResponse.next();
+
+  return NextResponse.redirect(new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase()));
+};
+
+export default Middleware;
