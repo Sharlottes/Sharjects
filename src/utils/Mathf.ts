94caa95bbcb018560b07580718075bfd9a52a998
Sharlotte <seeseafood@naver.com>
2023-03-11 15:31:08 +0900

    feat: add clamp util

--- src/utils/Mathf.ts
+++ src/utils/Mathf.ts
@@ -1,6 +1,9 @@
 namespace Mathf {
   export const lerp = (from: number, to: number, prog: number) =>
     from + (to - from) * prog;
+
+  export const clamp = (value: number, max = 1, min = 0) =>
+    Math.max(min, Math.min(max, value));
 }
 
 export default Mathf;
