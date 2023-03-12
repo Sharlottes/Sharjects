60ac737105c2887b4be9f5541efa525409c1f7d5
Sharlotte <seeseafood@naver.com>
2023-03-12 20:28:24 +0900

    feat: content 섹션 스타일 게선

--- src/components/pages/index/DescriptSection/Content/index.tsx
+++ src/components/pages/index/DescriptSection/Content/index.tsx
@@ -16,18 +16,20 @@ const Content: React.FC<ContentProps> = ({
   image,
   link,
   ...props
-}) => (
-  <ContentContainer image={image} {...props}>
-    <ContentBox>
-      <Typography variant="h5" fontWeight="bold">
-        {title}
-      </Typography>
-      <Typography variant="body2">{description}</Typography>
-    </ContentBox>
-    <EnterButton href={link} variant="contained">
-      {"> 들어가기"}
-    </EnterButton>
-  </ContentContainer>
-);
+}) => {
+  return (
+    <ContentContainer image={image} {...props}>
+      <ContentBox>
+        <Typography variant="h5" fontWeight="bold">
+          {title}
+        </Typography>
+        <Typography variant="body2">{description}</Typography>
+      </ContentBox>
+      <EnterButton href={link} variant="contained">
+        {"> 들어가기"}
+      </EnterButton>
+    </ContentContainer>
+  );
+};
 
 export default Content;
--- src/components/pages/index/DescriptSection/Content/styled.tsx
+++ src/components/pages/index/DescriptSection/Content/styled.tsx
@@ -1,3 +1,4 @@
+import { alpha } from "@mui/material";
 import Button from "@mui/material/Button";
 import styled from "@mui/system/styled";
 import { motion } from "framer-motion";
@@ -7,22 +8,28 @@ export const ContentContainer = styled(motion.div)<{
 }>(({ image }) => ({
   textAlign: "center",
   width: "100vw",
-  height: "250px",
+  height: "350px",
   backgroundImage: `url(${image})`,
-  backgroundPosition: "-110px 75px",
+  backgroundPosition: "-50px 400px",
   backgroundAttachment: "fixed",
   boxShadow: "inset 0 -50px 150px -50px #000",
 }));
 
 export const ContentBox = styled("div")(({ theme }) => ({
+  "--themedColor": theme.palette.mode === "light" ? "white" : "black",
   height: "100px",
   transform: "translateY(-50px)",
   backdropFilter: "blur(20px)",
-  backgroundColor: theme.palette.mode === "light" ? "#ffffffa8" : "#000000a8",
+  filter: `drop-shadow(16px 16px 20px var(--themedColor))`,
+  backgroundColor: alpha(
+    theme.palette.mode === "light" ? "#ffffff" : "#000000",
+    0.75
+  ),
   padding: "20px",
+  boxShadow: `0 0 30px var(--themedColor)`,
 }));
 
 export const EnterButton = styled(Button)({
   fontWeight: "bold",
-  transform: "translateY(40px)",
+  transform: "translateY(120px)",
 });
