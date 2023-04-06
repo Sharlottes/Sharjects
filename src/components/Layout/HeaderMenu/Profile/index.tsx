03dc0efb890f83e0aa9b7cd9a568cdf0c57a2cf2
Sharlotte <seeseafood@naver.com>
2023-04-07 07:21:35 +0900

    refactor: 왜 ProfileContainer가 여기있지..?

--- src/components/Layout/HeaderMenu/Profile/index.tsx
+++ src/components/Layout/HeaderMenu/Profile/index.tsx
@@ -6,13 +6,13 @@ import LoginIcon from "@mui/icons-material/Login";
 import LogoutIcon from "@mui/icons-material/Logout";
 
 import { signIn, signOut, useSession } from "next-auth/react";
-import { ProfileContainer } from "../../styled";
+import * as S from "./styled";
 
 const Profile: React.FC = () => {
   const { data: session, status } = useSession();
 
   return (
-    <ProfileContainer>
+    <S.ProfileContainer>
       <Avatar src={session?.user?.image ?? ""} />
       <Link href="/mypage">
         {session ? session.user?.name ?? "" : "not logged in!"}
@@ -26,7 +26,7 @@ const Profile: React.FC = () => {
           <LoginIcon />
         </IconButton>
       )}
-    </ProfileContainer>
+    </S.ProfileContainer>
   );
 };
 
--- src/components/Layout/styled.tsx
+++ src/components/Layout/styled.tsx
@@ -1,25 +1,5 @@
 import styled from "@mui/system/styled";
 
-export const ProfileContainer = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between",
-  whiteSpace: "nowrap",
-  margin: "0 10px",
-  width: "100%",
-  "& .MuiAvatar-root": {
-    padding: "4px",
-    margin: "4px",
-    width: "35px",
-    height: "35px",
-    backgroundColor: "rgba(0,0,0,0)",
-    color: theme.palette.mode === "dark" ? "white" : "black",
-  },
-  "& button": {
-    flex: 2,
-  },
-}));
-
 export const MainBody = styled("main")(({ theme }) =>
   theme.unstable_sx({
     minHeight: "100vh",
