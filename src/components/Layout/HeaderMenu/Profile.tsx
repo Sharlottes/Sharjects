aa3fdd382748ae1c345c14fa22b8fc92c1904309
Sharlotte <seeseafood@naver.com>
2023-04-07 08:30:37 +0900

    feat: show loading when use session

--- src/components/Layout/HeaderMenu/Profile.tsx
+++ src/components/Layout/HeaderMenu/Profile.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
+import CircularProgress from "@mui/material/CircularProgress";
+
 import LoginIcon from "@mui/icons-material/Login";
 import LogoutIcon from "@mui/icons-material/Logout";
 
@@ -10,6 +12,8 @@ import * as S from "./Profile.styled";
 
 const Profile: React.FC = () => {
   const { data: session, status } = useSession();
+
+  if (status == "loading") return <CircularProgress />;
 
   return (
     <S.ProfileContainer>
