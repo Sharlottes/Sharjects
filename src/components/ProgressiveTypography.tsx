271ab6cf386659087f97193020de57bb94ee7fb2
Sharlotte <aaa9810321@gmail.com>
2023-10-20 03:09:21 +0900

    refactor: optimized progressive typography

--- src/components/ProgressiveTypography.tsx
+++ src/components/ProgressiveTypography.tsx
@@ -1,70 +1,90 @@
-import React from "react";
-
+import { useRef, useEffect } from "react";
 import Box, { type BoxProps } from "@mui/material/Box";
-import Typography, { type TypographyProps } from "@mui/material/Typography";
-import {
-  type AnimationControls,
-  type MotionProps,
-  motion,
-  useAnimationControls,
-} from "framer-motion";
+import { motion, useAnimationControls } from "framer-motion";
 
-import U from "./ProgressiveTypography.util";
-
-type MotionPropsGetterType = (
-  char: string,
-  ref: React.RefObject<HTMLDivElement>,
-  control: (x: string | number, y: string | number) => Promise<any>
-) => MotionProps | undefined;
-
-interface ProgressiveTypographyProps extends TypographyProps {
+interface ProgressiveTypographyProps
+  extends Pick<AnimationCharProps, "speed" | "delay">,
+    BoxProps {
   label: string;
-  speed?: number | undefined;
-  delay?: number;
-  motion?: MotionPropsGetterType;
-  boxProps?: BoxProps | undefined;
 }
-
-const ProgressiveTypography: React.FC<ProgressiveTypographyProps> = ({
+export default function ProgressiveTypography({
   label,
-  delay = 0,
-  speed = 0.1,
-  motion: motionProps = U.motionPropsGenerator,
-  boxProps,
+  delay,
+  speed,
   ...props
-}) => {
-  const control: AnimationControls[] = [];
-
+}: ProgressiveTypographyProps) {
   return (
-    <Box style={{ display: "flex" }} {...boxProps}>
+    <Box display="flex" {...props}>
       {label.split("").map((char, i) => {
-        const ref = React.useRef<HTMLDivElement>(null);
-        const c = useAnimationControls();
-        const cc = useAnimationControls();
-        control[i] = c;
-
         return (
-          <motion.div key={i} animate={cc}>
-            <motion.div
-              custom={i}
-              initial={{ opacity: 0 }}
-              animate={c}
-              variants={{
-                show: {
-                  opacity: 1,
-                  transition: { delay: i * speed + delay, duration: 0.3 },
-                },
-              }}
-              ref={ref}
-              {...motionProps(char, ref, (x, y) => cc.start({ x, y }))}
-            >
-              <Typography {...props}>{char}</Typography>
-            </motion.div>
-          </motion.div>
+          <AnimationChar
+            char={char}
+            delay={delay}
+            speed={speed}
+            index={i}
+            key={i}
+          />
         );
       })}
     </Box>
   );
-};
+}
 
-export default ProgressiveTypography;
+interface AnimationCharProps {
+  speed?: number | undefined;
+  delay?: number | undefined;
+  index: number;
+  char: string;
+}
+function AnimationChar({
+  index,
+  char,
+  delay = 0,
+  speed = 0.1,
+}: AnimationCharProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const control = useAnimationControls();
+
+  const handleDragEnd = () => {
+    const data = isScreenOut(ref);
+    if (!data) return;
+    control.start({ x: 0, y: 0, transition: { delay: 1 } }).then(() => {
+      if (char == "o") {
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+      }
+    });
+  };
+
+  useEffect(() => {
+    control.start({
+      opacity: 1,
+      transition: { delay: index * speed + delay, duration: 0.3 },
+    });
+  }, []);
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0 }}
+      animate={control}
+      drag
+      dragTransition={{ power: 0.4, timeConstant: 200 }}
+      onDragEnd={handleDragEnd}
+      onDragTransitionEnd={handleDragEnd}
+    >
+      <p>{char}</p>
+    </motion.div>
+  );
+}
+function isScreenOut(ref: React.RefObject<HTMLDivElement>): boolean {
+  if (!ref.current) return false;
+
+  const rect = ref.current.getBoundingClientRect();
+  const isInOfViewPoint =
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+  return !isInOfViewPoint;
+}
