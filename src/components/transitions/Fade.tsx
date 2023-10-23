fe964335083b43c813796ba50fc5832fa1e5e4c1
Sharlotte <aaa9810321@gmail.com>
2023-10-24 04:13:49 +0900

    refactor: remove unused & useless components

--- src/components/transitions/Fade.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import MUIFade from "@mui/material/Fade";
-import type { MUISafeTransitionProps } from "src/@types/local";
-
-const Fade = React.forwardRef<unknown, MUISafeTransitionProps>((props, ref) => (
-  <MUIFade ref={ref} {...props} />
-));
-
-export default Fade;
